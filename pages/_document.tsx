d16a34c4120d16c6b54be145b8ddfb2bb1849fa6
Goulin Khoge <goulin.khoge@gmail.com>
2021-03-02 02:41:18 +0100

    Google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,7 +18,23 @@ class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="fr">
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-D8LXJYVYY2"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-D8LXJYVYY2');
+        `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
